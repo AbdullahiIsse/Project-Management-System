ca1d5ad8196b47cef63897d78b205a1167b69ab1
abdullahiisse <abdullahi12351@gmail.com>
2021-09-24 22:45:40 +0200

    Spring Integration Testing

--- src/main/java/com/ai/projectmanagement/entities/Employee.java
+++ src/main/java/com/ai/projectmanagement/entities/Employee.java
@@ -18,7 +18,6 @@ public class Employee {
     @ManyToMany(cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.REFRESH,CascadeType.PERSIST}
     , fetch = FetchType.LAZY)
     @JoinTable(name = "project_employee",joinColumns = @JoinColumn(name = "employee_id"),inverseJoinColumns =@JoinColumn(name = "project_id") )
-
     private List<Project> project;
 
     public Employee() {
