c8202536c8f54e1ccde273f6e17feec27fb5e892
Abdllahi isse <abdullahi12351@gmail.com>
2021-10-06 16:00:22 +0200

    test

---
+++ src/main/java/com/ai/projectmanagement/controller/APIController.java
@@ -0,0 +1,41 @@
+package com.ai.projectmanagement.controller;
+
+
+import com.ai.projectmanagement.dao.EmployeeRepository;
+import com.ai.projectmanagement.dao.ProjectRepository;
+import com.ai.projectmanagement.entities.Employee;
+import com.ai.projectmanagement.entities.Project;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+
+public class APIController {
+
+    @Autowired
+    EmployeeRepository empRepo;
+
+    @GetMapping("/test")
+    public ResponseEntity<Lisa<t<Employee>> getproject(){
+
+        List<Employee> employeeList = empRepo.findAll();
+
+        return new ResponseEntity<List<Employee>>(employeeList, HttpStatus.OK);
+
+
+    }
+
+
+
+
+
+
+
+}
